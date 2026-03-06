de650b0eeefb3cb008b521afdddc056d45faa318
Noah Adams <noahgadams07@gmail.com>
2026-03-06 00:17:40 -0500

    Started working on frontend

--- hardware/sensor.py
+++ hardware/sensor.py
@@ -1,1 +1,9 @@
-print("hello world")
+import time
+
+from w1thermsensor import W1ThermSensor
+
+temp_sensor = W1ThermSensor()
+
+while True:
+    print(temp_sensor.get_temperature())
+    time.sleep(1)
