9cc0b40ad00f4b757b760453996f00645036cf35
Noah Adams <noahgadams07@gmail.com>
2026-03-08 23:52:57 -0400

    set video stream to muted explicetly, to hopefully fix stream being black if due to autoplay blocking it.

--- web/frontend/js/stream.js
+++ web/frontend/js/stream.js
@@ -7,6 +7,8 @@ let hls = null;
 function loadVideoStream(url) {
     const video = document.getElementById("video-player");
 
+    video.muted = true;
+
     // clean up the old stream if one exists
     if (hls) {
         hls.destroy();
